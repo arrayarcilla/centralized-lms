d8f954babef5cb83af74ff1381024931311ab3a0
Shinji Ishikawa <shinG.ishikawa@gmail.com>
2024-03-16 16:45:41 +0800

    Added dependecies for the server side

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
-const app = express();
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
+const app = express();
 const port = 3000;
+
+const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "inventory"
+})
+
+app.get('/', (req, res) => {
+    res.send("hello world")
+})
 
 app.listen(port, () => {
     console.log('Server listening on port' + port);
