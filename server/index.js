8271a47999462741af7d454093e0124d734b55ad
Shinji Ishikawa <shinG.ishikawa@gmail.com>
2024-03-19 19:15:26 +0800

    Created Login and signup function

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
+const session = require("express-session");
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded()); //Parse URL-encoded bodies
+app.use(express.urlencoded());
 
 const app = express();
 const port = 3000;
+
+app.use(session({
+    secret: 'ssshhhhh', //random string i guess
+    resave: true,
+    saveUninitialized: false //https://github.com/expressjs/session#options <-- useful site
+}));
 
 const connection = mysql.createConnection({
     host: "127.0.0.1",
@@ -15,8 +23,34 @@ const connection = mysql.createConnection({
     database: "inventory"
 })
 
-app.get('/', (req, res) => {
-    res.send("hello world")
+app.post('/', urlEncodedParser, (req, res) => {
+    sesh = req.session;
+    connection.query('SELECT * FROM user WHERE username="' + req.body.user + '"', (err, result) => {
+        if (result.length != 0 && bcrypt.compareSync(req.body.pwd, result[0]['password'])) {
+            sesh.id = result[0].id;
+            sesh.user = result[0].username;
+            sesh.usertype = result[0].usertype;
+             if(result[0].usertype === 1){
+                res.send("member");
+             }else{
+                res.send("admin");
+             }
+            // res.send('dashboard');
+        } else {
+            res.send(" ");
+            throw err;
+
+        }
+    })
+})
+
+app.post('/create_user', (req, res) => {
+    console.log(req.query);
+    let salt = bcrypt.genSaltSync(saltRounds);
+    let hash = bcrypt.hashSync(req.query.password, salt);
+    connection.query('INSERT INTO user (username, password, usertype) VALUES ("' + req.query.username + '","' + hash + '","' + req.query.usertype + '")', (err, result) => {
+        if (err) throw err
+    })
 })
 
 app.listen(port, () => {
